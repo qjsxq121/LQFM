15052a8a9b1e020191c79dc26004bcdbe03026b1
李强 <liqiang@mammoth.ink>
2020-04-19 22:18:03 +0800

    播放传递数据

--- LQFM/Function/Player/LQRemotePlayer.h
+++ LQFM/Function/Player/LQRemotePlayer.h
@@ -9,7 +9,24 @@
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
-
+/**
+ * 播放器的状态
+ * 因为UI界面需要加载状态显示, 所以需要提供加载状态
+ - XMGRemotePlayerStateUnknown: 未知(比如都没有开始播放音乐)
+ - XMGRemotePlayerStateLoading: 正在加载()
+ - XMGRemotePlayerStatePlaying: 正在播放
+ - XMGRemotePlayerStateStopped: 停止
+ - XMGRemotePlayerStatePause:   暂停
+ - XMGRemotePlayerStateFailed:  失败(比如没有网络缓存失败, 地址找不到)
+ */
+typedef NS_ENUM(NSInteger, LQRemotePlayerState) {
+    LQRemotePlayerStateUnknown = 0,
+    LQRemotePlayerStateLoading   = 1,
+    LQRemotePlayerStatePlaying   = 2,
+    LQRemotePlayerStateStopped   = 3,
+    LQRemotePlayerStatePause     = 4,
+    LQRemotePlayerStateFailed    = 5
+};
 @interface LQRemotePlayer : NSObject
 
 + (instancetype)share;
@@ -41,6 +58,43 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)resume;
 // 停止
 - (void)stop;
+
+
+#pragma mark -- 数据提供
+// 推模式
+// 拉模式
+/** 静音 */
+@property (nonatomic, assign) BOOL muted;
+
+/** 声音 */
+@property (nonatomic, assign) float volume;
+
+/** 速率 */
+@property (nonatomic, assign) float rate;
+
+/** 总时长 */
+@property (nonatomic, assign, readonly) NSTimeInterval totalTime;
+
+/** 总时间 */
+@property (nonatomic, copy, readonly) NSString *totalTimeFormat;
+
+/** 当前时长 */
+@property (nonatomic, assign, readonly) NSTimeInterval currentTime;
+
+/** 当前播放的时间 */
+@property (nonatomic, copy, readonly) NSString *currentTimeFormat;
+
+/** 进度 */
+@property (nonatomic, assign, readonly) float progress;
+/** 地址 */
+@property (nonatomic, assign, readonly) NSURL *url;
+/** 加载进度 */
+@property (nonatomic, assign, readonly) float loadDataProgress;
+
+/** 状态 */
+@property (nonatomic, assign) LQRemotePlayerState state;
+
+
 @end
 
 NS_ASSUME_NONNULL_END
--- LQFM/Function/Player/LQRemotePlayer.m
+++ LQFM/Function/Player/LQRemotePlayer.m
@@ -10,7 +10,9 @@
 #import <AVFoundation/AVFoundation.h>
 
 @interface LQRemotePlayer ()
-
+{
+    BOOL _isUserPause; // 是否是用户主动停止
+}
 /** 播放 */
 @property (nonatomic, strong) AVPlayer *player;
 
@@ -35,7 +37,13 @@ static LQRemotePlayer * _instance;
 }
 
 - (void)playWithURL:(NSURL *)url {
-    // 创建一个播放d器对象
+    NSURL *currentURL = [(AVURLAsset *)self.player.currentItem.asset URL];
+    if ([url isEqual:currentURL]) {
+        NSLog(@"当前播放任务已经存在");
+        [self resume];
+        return;
+    }
+    // 创建一个播放器对象
     // 步骤：
     //1 资源的请求
     //2 资源的组织
@@ -44,6 +52,11 @@ static LQRemotePlayer * _instance;
     // 如果资源加载比较慢，有可能，会造成调用了paly方法
     // 但是当前并没有播放音频
     
+    _url = url;
+    
+    if (self.player.currentItem) {
+        [self removeObserver];
+    }
     //1 资源的请求
     AVURLAsset *asset = [AVURLAsset assetWithURL:url];
     // 2 资源的组织
@@ -51,7 +64,13 @@ static LQRemotePlayer * _instance;
 
     // 当前资源的组织者，告诉我们资源准备好了之后，我们再播放
     [item addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
+    [item addObserver:self forKeyPath:@"playbackLikelyToKeepUp" options:NSKeyValueObservingOptionNew context:nil];
     
+    // 播放结束的监听
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playEnd) name:AVPlayerItemDidPlayToEndTimeNotification object:nil];
+    
+    // 播放被打断的监听
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playInterupt) name:AVPlayerItemPlaybackStalledNotification object:nil];
     self.player = [AVPlayer playerWithPlayerItem:item];
 }
 
@@ -101,24 +120,54 @@ static LQRemotePlayer * _instance;
 
 - (void)pause {
     [self.player pause];
+    _isUserPause = YES;
+    if (self.player) {
+        self.state = LQRemotePlayerStatePause;
+    }
 }
 - (void)resume {
     [self.player play];
+    _isUserPause = NO;
+    // 就是代表，当前播放器存在，并且 数据组织者里面的数据准备，已经足够播放了
+    if (self.player && self.player.currentItem.playbackLikelyToKeepUp) {
+        self.state = LQRemotePlayerStatePlaying;
+    }
 }
 - (void)stop {
     [self.player pause];
     self.player = nil;
+    if (self.player) {
+        self.state = LQRemotePlayerStateStopped;
+    }
+}
+// 移除监听
+- (void)removeObserver {
+    [self.player.currentItem removeObserver:self forKeyPath:@"status"];
+    [self.player.currentItem removeObserver:self forKeyPath:@"playbackLikelyToKeepUp"];
+}
+
+
+
+#pragma mark -- 数据 事件
+- (float)rate {
+    return self.player.rate;
 }
 // 设置速率
 - (void)setRate:(float)rate {
     [self.player setRate:rate];
 }
 
+- (BOOL)muted {
+    return self.player.muted;
+}
 // 是否静音
 - (void)setMuted:(BOOL)muted {
     self.player.muted = muted;
 }
 
+- (float)volume {
+    return self.player.volume;
+}
 - (void)setVolume:(float)volume {
     if (volume < 0 || volume > 1) {
         return;
@@ -129,16 +178,100 @@ static LQRemotePlayer * _instance;
     }
     self.player.volume = volume;
 }
+
+- (NSString *)currentTimeFormat {
+    return [NSString stringWithFormat:@"%2f:%2zd",(int)self.currentTime / 60,(int)self.currentTime % 60];
+}
+
+- (NSString *)totalTimeFormat {
+    return [NSString stringWithFormat:@"%2zd:%2zd",(int)self.totalTime / 60,(int)self.totalTime % 60];
+}
+
+-(NSTimeInterval)totalTime {
+    CMTime totalTime = self.player.currentItem.duration;
+    NSTimeInterval totalTimeSec = CMTimeGetSeconds(totalTime);
+    if (isnan(totalTimeSec)) {
+        return 0;
+    }
+    return totalTimeSec;
+}
+
+- (NSTimeInterval)currentTime {
+    CMTime playTime = self.player.currentItem.currentTime;
+    NSTimeInterval playTimeSec = CMTimeGetSeconds(playTime);
+    if (isnan(playTimeSec)) {
+        return 0;
+    }
+    return playTimeSec;
+}
+
+- (float)progress {
+    if (self.totalTime == 0) {
+        return 0;
+    }
+    return self.currentTime / self.totalTime;
+}
+
+- (float)loadDataProgress {
+    if (self.totalTime == 0) {
+        return 0;
+    }
+    CMTimeRange timeRange = [[self.player.currentItem loadedTimeRanges].lastObject CMTimeRangeValue];
+       
+       CMTime loadTime = CMTimeAdd(timeRange.start, timeRange.duration);
+       NSTimeInterval loadTimeSec = CMTimeGetSeconds(loadTime);
+       
+       return loadTimeSec / self.totalTime;
+}
+
+- (void)setState:(LQRemotePlayerState)state {
+    _state = state;
+    // 如果需要告诉外界相关事件
+    // block
+    // 代理
+    // 通知
+}
+// 播放结束
+- (void)playEnd {
+    NSLog(@"播放完成");
+    self.state = LQRemotePlayerStateStopped;
+    
+}
+
+- (void)playInterupt {
+    // 来电话，资源加载跟不上
+    NSLog(@"播放被打断");
+    self.state = LQRemotePlayerStatePause;
+}
 #pragma mark -- KVO
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
     if ([keyPath isEqualToString:@"status"]) {
         AVPlayerItemStatus status = [change[NSKeyValueChangeNewKey] integerValue];
         if (status == AVPlayerItemStatusReadyToPlay) {
             NSLog(@"资源准备好了，可以播放");
-            [self.player play];
+            [self resume];
         } else {
             NSLog(@"状态未知");
+            self.state = LQRemotePlayerStateFailed;
         }
+    } else if ([keyPath isEqualToString:@"playbackLikelyToKeepUp"]) {
+         BOOL ptk = [change[NSKeyValueChangeNewKey] boolValue];
+                if (ptk) {
+                    NSLog(@"当前的资源, 准备的已经足够播放了");
+        //
+                    // 用户的手动暂停的优先级最高
+                    if (!_isUserPause) {
+                        [self resume];
+                    }else {
+                        
+                    }
+                    
+                }else {
+                    NSLog(@"资源还不够, 正在加载过程当中");
+                    self.state = LQRemotePlayerStateLoading;
+                }
     }
 }
+
+
 @end
